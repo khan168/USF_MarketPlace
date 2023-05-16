669ccbc41fe79d111e0ea1b3c0d0a1766a1c69ff
khan162 <umarahmedkhank6@gmail.com>
2023-05-15 23:52:11 -0400

    deleted files

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -6,19 +6,7 @@ import Categories from "../components/Categories";
 import Products from "../components/Products";
 import Newsletter from "../components/NewsLetter";
 import Footer from "../components/Footer";
-import { styled } from "styled-components";
 
-const Container = styled.div`
-  position: relative;
-  width: 100%;
-  height: 80vh;
-  display: flex;
-  /* background-color: #b98c12; */
-  margin: auto;
-  overflow: hidden;
-  background-image: url("ezgif-2-b778203b7e.jpg");
-  background-size: contain;
-`;
 
 const Home = () => {
   return (

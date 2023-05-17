f2a2b5630a3f64a56a1a7e0d735d7c9336191fec
khan162 <umarahmedkhank6@gmail.com>
2023-05-16 23:15:29 -0400

    react-router added

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,12 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./app";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import SingleProduct from "./pages/SingleProduct";
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
 
+// import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/list",
+    element: <ProductList />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/single",
+    element: <SingleProduct />,
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
   document.getElementById("root")
 );

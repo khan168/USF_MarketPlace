3a263daa3787f74056f6713084f5c556dcc20121
khan162 <umarahmedkhank6@gmail.com>
2023-05-16 23:24:24 -0400

    fixing the root app component rendering

--- my-react-app/src/app.jsx
+++ my-react-app/src/app.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import ProductList from './pages/ProductList'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import SingleProduct from "./pages/SingleProduct";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/list",
+    element: <ProductList />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/single",
+    element: <SingleProduct />,
+  },
+]);
 
 export const App = () => {
-  return (
-    <ProductList></ProductList>
-  )
+  return <RouterProvider router={router} />;
 }
--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,47 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import ProductList from "./pages/ProductList";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import SingleProduct from "./pages/SingleProduct";
+import { App } from "./app";
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
 
-// import Root from "./routes/root";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/list",
-    element: <ProductList />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/single",
-    element: <SingleProduct />,
-  },
-]);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
+

8911f6c15b1856b8f5b14058ec6d8a2ae4ce730b
khan162 <umarahmedkhank6@gmail.com>
2023-05-21 19:07:19 -0400

    hiding db key

--- api/backend/config/db.js
+++ api/backend/config/db.js
@@ -7,7 +7,7 @@ const connectDB = async () => {
     mongoose.set("strictQuery", true);
     try {
         const conn = await mongoose.connect(
-          "mongodb+srv://nathannoelallen:T8pDA3tSb3ookFZz@bullscluster.kh6xutx.mongodb.net/bullsapp"
+          process.env.MONGO_URI
         );
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {

db43618998c98c175dc843f832319ea0cd3eda24
khan162 <umarahmedkhank6@gmail.com>
2023-07-01 15:36:18 -0400

    more css changes

--- my-react-app/src/components/Footer.jsx
+++ my-react-app/src/components/Footer.jsx
@@ -4,28 +4,27 @@ import styled from "styled-components";
 
 const Container = styled.div`
   align-items: center;
-  margin-left: 12vw;
   display: flex;
   /* flex-direction: column; */
   background-color: rgb(0, 103, 71);
   color: white;
-  
-  
-`
+`;
 
 const Left = styled.div`
   flex: 2;
   display: flex;
   flex-direction: column;
   padding: 30px;
- 
+  @media (max-width: 768px) {
+    display: none;
+  }
 `;
 
 const Logo = styled.h1``;
 
 const Desc = styled.p`
   margin: 20px 0px;
-`;
+`
 
 const Center = styled.div`
   height: 50px;
@@ -35,7 +34,10 @@ const Center = styled.div`
   align-items: center;
   text-align: center;
   margin-bottom: 20px;
-  
+  @media (max-width: 768px) {
+    height: 30vh;
+    
+  }
 `;
 
 const GitIcon = styled.h3`
@@ -49,8 +51,8 @@ const List = styled.ul`
   padding: 0;
   list-style: none;
   display: flex;
-  
-`
+  margin-left: 18vw;
+`;
 
 
 
@@ -66,25 +68,13 @@ const Footer = () => {
           was crafted with love and keen understanding of university life. Our
           goal? To simplify your campus journey. Enjoy browsing!
         </Desc>
-        {/* <SocialContainer>
-          <SocialIcon color="3B5999">
-            <FacebookIcon />
-          </SocialIcon>
-          <SocialIcon color="E4405F">
-            <InstagramIcon />
-          </SocialIcon>
-          <SocialIcon color="55ACEE">
-            <TwitterIcon />
-          </SocialIcon>
-          <SocialIcon color="E60023">
-            <PinterestIcon />
-          </SocialIcon>
-        </SocialContainer> */}
       </Left>
       <Center>
         <List>
           <GitHubIcon></GitHubIcon>
-          <GitIcon>Github</GitIcon>
+          <GitIcon>
+            <a href="https://github.com/khan168/USF_MarketPlace" target="blank" style={{textDecoration:"none", color:"white"}}>Github</a>
+          </GitIcon>
         </List>
       </Center>
     </Container>
